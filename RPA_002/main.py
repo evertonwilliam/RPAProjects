2d3994c894b9e232798e8ff5034e826abe01b9a2
Everton William <evertonwilliam@gmail.com>
2021-11-26 09:09:04 -0300

    Versão Final

--- RPA_002/main.py
+++ RPA_002/main.py
@@ -23,22 +23,14 @@ import threading
 import os
 import rarfile
 import pyautogui
+import logging
+import logzero
+from logzero import logger
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
-
-#CONECTANDO EM UM BANCO DE DADOS SQLITE
-#conn = sqlite3.connect('bot.db')
-
-#Criando a tabela do banco de dados
-#cur = conn.cursor()
-#cur.execute('Create table if not exists bot(pergunta TEXT NULL, resposta TEXT NULL);') 
-#cur.close()
-#conn.close()
-
 
 # PASSO 01 - DEFINIÇÃO DE VARIÁVEIS
 urlSite = 'https://mundorpa.com/index.html'
@@ -47,12 +39,17 @@ dirLog  = '\log'
 dirExe  = '\exe'
 fileDw  = '\MundoRPA - Desafio App.rar'
 fileEx  = '\MundoRPA - Desafio App.exe'
+fileLogName = dirProj + dirLog + '\log.txt'
 
+# Definição do processo de logs
+logzero.logfile(fileLogName, maxBytes=1e6, backupCount=1, disableStderrLogger=False)
+logger.setLevel(logging.DEBUG) 
 
 # FUNÇÃO QUE CRIA PASTAS NO WINDOWS
 def createFolders (folder):
     try:
         if os.path.exists(dirProj+folder) == False:
+            logger.debug(':: Executar comando de criar diretórios')
             os.makedirs(dirProj+folder)
     except Exception as ex:
         print (ex)
@@ -62,6 +59,7 @@ def createFolders (folder):
 def fileVerification(folderName, fileName):
     try:
         if os.path.exists(dirProj+folderName+fileName) == True:
+            logger.debug(':: Executar o comando remove file')
             os.remove(dirProj+folderName+fileName)
     except Exception as ex:
         print(ex)
@@ -70,6 +68,7 @@ def fileVerification(folderName, fileName):
 # FUNCAO QUE INICIALIZA O NAVEGADOR
 def initNavChrome(url):
     try:
+        logger.debug(':: Iniciando o navegador e seus dados de navegação')
         options = webdriver.ChromeOptions()
         #prefs = {"download.default_directory" : dirProj+dirExe}
         prefs = {
@@ -85,16 +84,19 @@ def initNavChrome(url):
         chrome.get(url)
         return chrome
     except Exception as ex:
+        logger.debug(':: Definir os dados do navegador chrome: '+ ex)
         print(ex)
         exit()
 
 # FUNÇÃO QUE AGUARDA UM ELEMENTO EM TELA     
 def waitElement (driver, type, object, time):
     try:
+        logger.debug(':: Aguardar o elemento: ' + object)
         wait = WebDriverWait(driver, time)
         element = wait.until(EC.element_to_be_clickable((type, object)))
         return element
     except Exception as ex:
+        logger.debug(':: Erro ao aguardar elemento: ' + ex)
         print(ex)
         exit()
 
@@ -102,7 +104,7 @@ def waitElement (driver, type, object, time):
 def getDownLoadedFileName(driver):
     driver.switch_to.window(driver.window_handles[-1])
     driver.get('chrome://downloads')
-
+    logger.debug(':: Aguardar o download ocorrer')
     # CRIANDO A ANIMAÇÃO DE CARREGAMENTO
     for c in itertools.cycle(['|', '/', '-', '\\']):
         try:
@@ -126,7 +128,28 @@ def getDownLoadedFileName(driver):
         finally:
             # CASO FINALIZE O DOWNLOAD, CONTINUA O PROCESSO
             if downloadPercentage == 100:
-               
+                sys.stdout.write('\r')
+                logger.debug(':: Confirmar o download do arquivo do site')
+                time.sleep(1)
+                pyautogui.hotkey('TAB')
+                time.sleep(1)
+                pyautogui.hotkey('TAB')
+                time.sleep(1)
+                pyautogui.hotkey('TAB')
+                time.sleep(1)
+                pyautogui.hotkey('TAB')
+                time.sleep(1)
+                pyautogui.hotkey('TAB')  
+                time.sleep(1)
+                pyautogui.hotkey('TAB') 
+                time.sleep(1)
+                pyautogui.hotkey('ENTER')
+                time.sleep(2)
+                pyautogui.hotkey('TAB')                
+                time.sleep(1)
+                pyautogui.hotkey('ENTER')
+                time.sleep(5)
+
                 driver.back()
                 return 'Concluido'
             
@@ -136,14 +159,11 @@ def getDownLoadedFileName(driver):
 
 # FUNCÃO QUE LÊ A TABELA DO SITE
 def getTabelaWeb(driver, object):
-    #table =  waitElement(driver, By.XPATH, object, 30)
+    logger.debug(':: Ler os dados da tabela do site')
     itens = []
     for row in object.find_elements(By.XPATH, ".//tr"):
         itens.append([td.text for td in row.find_elements(By.XPATH, ".//td")])
     return itens
-
-
-
 
 # EXECUTANDO O PROCESSO
 if(__name__) == '__main__':
@@ -153,27 +173,30 @@ if(__name__) == '__main__':
     createFolders(dirExe)
     
     # verifica os downloads
-    #fileVerification(dirExe, fileDw)
+    fileVerification(dirExe, fileDw)
     
     # Inicializa o navegador
     nav = initNavChrome(urlSite)
     
     # MAPEAMENTO
+    logger.debug(':: Organizar as telas no desktop')
+    pyautogui.hotkey('win', 'left')
+    pyautogui.hotkey('esc')
  
     # LINK DESAFIOS
     linkDesafio = waitElement(nav, By.LINK_TEXT, 'Desafios', 40)
     linkDesafio.click()
 
     # LINK DE DOWNLOADS
-    #linkDownload = waitElement(nav, By.LINK_TEXT, 'Aplicação Desktop - Input', 30)
-    #linkDownload.click()
+    linkDownload = waitElement(nav, By.LINK_TEXT, 'Aplicação Desktop - Input', 30)
+    linkDownload.click()
 
     # AGUARDANDO O DOWNLOAD E VERIFICANDO SE JÁ BAIXOU TUDO
-    #if getDownLoadedFileName(nav) != 'Concluido':
-        #print('Erro de Download')
-        #nav.close()
-        #exit()
-    
+    if getDownLoadedFileName(nav) != 'Concluido':
+        logger.debug(':: Erro de Download')
+        nav.close()
+        exit()
+
     # LINK TABELA DE DADOS
     inkDesafio = waitElement(nav, By.LINK_TEXT, 'DESAFIO 4', 30)
     inkDesafio.click()
@@ -183,10 +206,12 @@ if(__name__) == '__main__':
     dados = getTabelaWeb(nav, webTable)
     
     #FECHA NAVEGADOR
+    logger.debug(':: Fechar o navegador')
     nav.close()
 
     # DESCOMPACTA O EXECUTÁVEL BAIXADO
     try:
+        logger.debug(':: Descompactar o arquivo')
         r = rarfile.RarFile(dirProj+dirExe+fileDw)
         r.extractall(dirProj+dirExe)
         r.close()
@@ -196,6 +221,7 @@ if(__name__) == '__main__':
 
     # EFETUA O CADASTRO
     try:
+        logger.debug(':: Iniciar processo de cadastro no sistema')
         pyautogui.PAUSE = 0.1
         pyautogui.hotkey('winleft', 'r')
         pyautogui.hotkey('DEL')
@@ -207,6 +233,7 @@ if(__name__) == '__main__':
         pyautogui.hotkey('TAB')    
     
         for i in range(len(dados)):
+            logger.debug(':: Cadastrar o material: ' + dados[i][0])
             pyautogui.hotkey('TAB')
             pyautogui.hotkey('TAB')
             pyautogui.hotkey('TAB')    
@@ -220,18 +247,14 @@ if(__name__) == '__main__':
             pyautogui.hotkey('SPACE')
             
     except Exception as ex:
+        logger.debug(':: Ocorreu erro no cadastro:' + ex)
         print(ex)
         exit()
     finally:
+        logger.debug(':: ENCERRANDO O PROCESSO')
         pyautogui.hotkey('TAB')
         pyautogui.hotkey('TAB')
         pyautogui.hotkey('SPACE')
         pyautogui.hotkey('SPACE')        
         pyautogui.hotkey('ALT', 'F4')
         pyautogui.alert('Fim do Processo')
-    
-    
-    
-    
-    
-    
